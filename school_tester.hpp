49a076b39b6c98fa2afb83c4f13659f3fe2f50f7
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-08-08 19:15:24 -0400

    Update school_tester.hpp

--- school_tester.hpp
+++ school_tester.hpp
@@ -5,4 +5,34 @@
 #error "Only intended to be included in C++ files"
 #endif
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <fstream>
+
+namespace st {
+    namespace detail {
+        class TestRunner {
+        public:
+            using TestFunc = void(*)();
+
+            static void addTest(const char* name, TestRunner::TestFunc f);
+
+        private:
+            static std::unordered_map<std::string, TestFunc> g_tests;
+        };
+
+
+        
+    }
+}
+
+namespace st {
+    namespace detail {
+        void TestRunner::addTest(const char* name, TestRunner::TestFunc f) {
+            g_tests.insert({std::string(name), f});
+        }
+    }
+}
+
 #endif // SCHOOL_TESTER_H_
